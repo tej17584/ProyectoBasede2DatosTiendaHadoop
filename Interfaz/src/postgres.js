f0b9664979931ab517dce909b562197a5eb07631
Andres Urizar <uri17632@uvg.edu.gt>
2019-05-13 15:32:59 -0600

    cambio de funcion
    funcion

---
+++ Interfaz/src/postgres.js
@@ -0,0 +1,30 @@
+var {Pool} = require('pg');
+
+var pool = new Pool({
+    user: 'postgres',
+    password: 'andresumsql',
+    database: 'proyectobasededatos'
+});
+
+async function agregarProducto(id,nombre,cantidad,precio,marca,categoria){
+    try {
+        var bandera = false;
+        await pool.connect().then(client =>{
+            client.query('select * from producto where id_producto = $1',[id]).then(res =>{
+                client.release()
+                // console.log(res.rows[0].id_producto)
+                if (res.rowCount == 0){
+                    bandera = true
+                }
+                if (bandera == true){
+                    console.log("coso");
+                }
+            })
+        })
+        if (bandera == true){
+            console.log("Funciono")
+        }
+    } catch (ex) {
+        console.log("No se pudo agregar a la base de datos")
+    }
+}
