2a09384baa5b40ec925373b70abedf6f03b3677c
Andres Urizar <uri17632@uvg.edu.gt>
2019-05-13 14:53:24 -0600

    Inicio de funciones de postgres
    Ingresar producto

--- Interfaz/src/postgres.js
+++ Interfaz/src/postgres.js
@@ -1,8 +1,24 @@
 var {Pool} = require('pg');
 
 var pool = new Pool({
-    database: 'lab_09',
     user: 'postgres',
     password: 'andresumsql',
-    port: 5432
+    database: 'proyectobasededatos'
 });
+
+async function agregarProducto(id,nombre,cantidad,precio,marca,categoria){
+    try {
+        var bandera = false;
+        await pool.connect().then(client =>{
+            client.query('select * from producto where id_producto = $1',[id]).then(res =>{
+                client.release()
+                // console.log(res.rows[0].id_producto)
+                if (res.rowCount == 0){
+                    bandera = true
+                }
+            })
+        })
+    } catch (ex) {
+        console.log("No se pudo agregar a la base de datos")
+    }
+}
