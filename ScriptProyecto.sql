e79cd9889afacb594ab8bfb54c8c973cfea22269
tej17584 <tej17584@uvg.edu.gt>
2019-05-16 14:10:32 -0600

    Update ScriptProyecto.sql
    Cambios en la generación de base de datos

--- ScriptProyecto.sql
+++ ScriptProyecto.sql
@@ -1,5 +1,4 @@
-create database proyectobasededatos
-
+create database proyectodb2;
 
 create table if not exists marca
 (
@@ -9,8 +8,6 @@ create table if not exists marca
     nombre  varchar(50)
 );
 
-
-
 create table if not exists categoria
 (
     idcategoria varchar(20) not null
@@ -19,7 +16,6 @@ create table if not exists categoria
     nombre      varchar(50),
     cantidad    integer
 );
-
 
 create table if not exists producto
 (
@@ -37,8 +33,6 @@ create table if not exists producto
             references categoria
 );
 
-
-
 create table if not exists definicion_datos_custom
 (
     id_def_dato_custom varchar(20) not null
@@ -48,30 +42,23 @@ create table if not exists definicion_datos_custom
     tipo               varchar(50)
 );
 
-
-
 create table if not exists datos_custom
 (
     id_def_dato_custom varchar(20)
         constraint datos_custom___fkdefdatoscustom
             references definicion_datos_custom,
-    id_producto        varchar(20)
-        constraint datos_custom___fkproducto
-            references producto,
+    id_producto        varchar(20),
     valor              varchar(50)
 );
 
-
-
 create table if not exists cliente
 (
-    dpi         varchar(20) not null
+    dpi       varchar(20) not null
         constraint cliente_pk
             primary key,
-    nombre      varchar(50),
-    "dirección" varchar(50)
+    nombre    varchar(50),
+    direccion varchar(50)
 );
-
 
 create table if not exists factura
 (
@@ -85,20 +72,15 @@ create table if not exists factura
     hora       time        not null
 );
 
-
-
 create table if not exists linea_factura
 (
-    id_linea_factura varchar(20) not null
+    -- Only integer types can be auto increment
+    id_linea_factura varchar(20) default nextval('linea_factura_id_linea_factura_seq'::regclass) not null
         constraint linea_factura_pk
             primary key,
-    id_factura       varchar(20)
-        constraint linea_factura___fkfactura
-            references factura,
+    id_factura       varchar(20),
     id_producto      varchar(20)
         constraint linea_factura___fkproducto
             references producto,
     cantidadcomprada integer
 );
-
-
